30b9a46c767d2b66ea2c09f20453bd84a5c12f90
이원호 <dldnjsgh71@naver.com>
2020-09-08 14:32:03 +0900

    :package:/mkdir dao,dto,service/

--- backend/src/main/java/com/project/IWillApplication.java
+++ backend/src/main/java/com/project/IWillApplication.java
@@ -1,13 +1,16 @@
 package com.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class IWillApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantApplication.class, args);
+		SpringApplication.run(IWillApplication.class, args);
 	}
 
 }
---
+++ backend/src/main/java/com/project/dto/Will.java
@@ -0,0 +1,12 @@
+package com.project.dto;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Will {
+
+    
+}
