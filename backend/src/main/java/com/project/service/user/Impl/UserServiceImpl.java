38b1af8022f12e6c92e788a5931d1d86f7bc347c
최현민 <hunmin8788@naver.com>
2020-09-10 01:40:59 +0900

    :up: / commit

---
+++ backend/src/main/java/com/project/service/user/Impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.project.service.user.Impl;
+
+import com.project.model.user.UserEntity;
+import com.project.service.user.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public UserEntity login(String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
