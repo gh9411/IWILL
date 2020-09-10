296cf2cd15906be1b057260aff32e2db462a6ddc
이원호 <dldnjsgh71@naver.com>
2020-09-10 10:20:45 +0900

    :new:/add app.json

--- backend/src/main/java/com/project/service/user/Impl/UserServiceImpl.java
+++ backend/src/main/java/com/project/service/user/Impl/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.project.service.user.Impl;
 import com.project.model.user.UserEntity;
 import com.project.service.user.UserService;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService{
 
 	@Override
