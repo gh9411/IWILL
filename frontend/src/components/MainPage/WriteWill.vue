5d8eeaabbe6df924c5276855b5a8925a88d14834
김종관 <colinder@naver.com>
2020-09-21 18:42:25 +0900

    :new: / mainpage edit /

--- frontend/src/components/MainPage/WriteWill.vue
+++ frontend/src/components/MainPage/WriteWill.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <h3>글 남기기</h3>
+  <div class="writeWILL">
+    <h3>기록 남기기</h3>
+    <md-field style="margin-top: 2em">
+      <label>제목</label>
+      <md-input v-model="Topic"></md-input>
+    </md-field>
+
+    <md-field>
+      <label>내용을 적어주세요</label>
+      <md-textarea v-model="textarea"></md-textarea>
+    </md-field>
+
+    <md-button class="md-primary">제출</md-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "writeWILL"
+  name: "writeWILL",
+  data() {
+    return {
+      Topic: "",
+      aboutWill: null
+    }
+  }
 }
 </script>
 
