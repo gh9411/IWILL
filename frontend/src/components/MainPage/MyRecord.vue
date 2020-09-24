11a7f89784574f5de8648098fb0053c401a9cc5f
김종관 <colinder@naver.com>
2020-09-24 11:55:50 +0900

    :new: / Will list page edit / S03P23A104-22

--- frontend/src/components/MainPage/MyRecord.vue
+++ frontend/src/components/MainPage/MyRecord.vue
@@ -7,7 +7,7 @@
       >
         <v-expansion-panel-header>Item</v-expansion-panel-header>
         <v-expansion-panel-content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -17,7 +17,7 @@
 <script>
 export default {
   name: "MyRecord"
-};
+}
 </script>
 
 <style></style>
